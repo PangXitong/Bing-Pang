662473f737054d1926ce013e1dab320dfe6b8edd
PangXitong <pangxitong@icloud.com>
2023-12-10 11:29:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,34 +3,38 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST', 'OPTIONS'])
-def proxy():
-    if request.method == 'OPTIONS':
-        # 处理预检请求
-        headers = {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-        }
-        return '', 200, headers
+@app.route('/', methods=['GET', 'POST'])
+def search():
+    if request.method == 'GET':
+        return '''
+            <html>
+                <body>
+                    <h1>Search Engine Proxy</h1>
+                    <form method="post" action="/">
+                        <input type="text" name="q" placeholder="Search...">
+                        <button type="submit">Search</button>
+                    </form>
+                </body>
+            </html>
+        '''
     else:
-        # 转发请求并处理响应
+        # 从表单获取查询字符串
+        query = request.form.get('q')
+
+        # 使用Google搜索
+        params = {
+            'q': query,
+            'num': 10
+        }
         headers = {
-            'Access-Control-Allow-Origin': '*'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
         }
         try:
-            response = requests.request(
-                method=request.method,
-                url='https://www.bing.com' + request.full_path,
-                headers={k: v for k, v in request.headers if k != 'Host'},
-                data=request.get_data(),
-                cookies=request.cookies,
-                allow_redirects=False
-            )
+            response = requests.get('https://www.google.com/search', params=params, headers=headers)
             content_type = response.headers.get('content-type')
-            return (response.content, response.status_code, {'Content-Type': content_type, 'Access-Control-Allow-Origin': '*'})
+            return (response.content, response.status_code, {'Content-Type': content_type})
         except requests.exceptions.RequestException as e:
-            return str(e), 500, headers
+            return str(e), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080)
